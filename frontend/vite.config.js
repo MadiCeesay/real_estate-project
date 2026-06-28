ed7fe3fc2d8f3ae0257a5d7bdd292c28441cdc72
Madi Ceesay <mc22326011@utg.edu.gm>
2026-06-28 14:47:02 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-  const backendUrl = env.VITE_API_URL ? env.VITE_API_URL.replace(/\/api\/v1\/?$/, '') : 'https://realestate-backend-jauj.onrender.com'
+  const backendUrl = env.VITE_API_URL ? env.VITE_API_URL.replace(/\/api\/v1\/?$/, '') : 
+  'https://realestate-backend-jauj.onrender.com'
 
   return defineConfig({
     plugins: [react()],
